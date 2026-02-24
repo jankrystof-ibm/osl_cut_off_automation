e84e591d2d85fdd581bbacdca748842cc3f6b094
jan krystof <jan.krystof@.ibm.com>
2026-02-24 09:25:17 +0100

    in progress

--- ansible/resolve_merge_conflicts.sh
+++ ansible/resolve_merge_conflicts.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+set -e
 
-git -C $1 checkout --ours .
-git -C $1 add .
-git -C $1 commit -m "Resolve conflicts keeping ours"
+# THIS MERGE STRATEGY IS NOT FOR PRODUCTION use
+
+REPO_PATH=$1
+
+# 1. get lit of all unmerged conflicts
+CONF_FILES=$(git -C "$REPO_PATH" diff --name-only --diff-filter=U)
+
+if [ -z "$CONF_FILES" ]; then
+    echo "no conflicts...
+fi
+
+# 2. check one by file
+echo "$CONF_FILES" | while read -r FILE; do
+    # try checkout --ours. if file is missing, pcommand selže.
+    if ! git -C "$REPO_PATH" checkout --ours "$FILE" 2>/dev/null; then
+        # if the command does not exist in our version => delete as we do not want it in OURS version
+        git -C "$REPO_PATH" rm "$FILE"
+    fi
+done
+
+# 3. complete
+git -C "$REPO_PATH" add .
+git -C "$REPO_PATH" commit -m "Resolve conflicts keeping ours"
