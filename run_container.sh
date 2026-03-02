0c0ce787e5d59447ae071856737d68c8769d61b9
Jan Krystof <jan.krystof@.ibm.com>
2026-03-02 19:02:43 +0100

    SRVLOGIC-807 | towards CI run

--- run_container.sh
+++ run_container.sh
@@ -46,8 +46,9 @@ ssh-keyscan github.com >> "$TMP_SSH_DIR/known_hosts" 2>/dev/null
 GIT_SSH_COMMAND="ssh -i $TMP_SSH_DIR/id_rsa -o UserKnownHostsFile=$TMP_SSH_DIR/known_hosts -o StrictHostKeyChecking=yes"
 
 export ANSIBLE_DIR_HOST=$ANSIBLE_DIR_HOST
-echo xxx
+echo xxx1
 echo "${ANSIBLE_DIR_HOST}:/tmp/osl_cut_off_automation/ansible:ro"
+ls -la ${ANSIBLE_DIR_HOST}
 docker run --rm \
   -e GIT_SSH_COMMAND="$GIT_SSH_COMMAND" \
   -v "${TMP_SSH_DIR}:${TMP_SSH_DIR}:ro" \
