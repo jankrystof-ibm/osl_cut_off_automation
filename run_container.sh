23b7b0c0e01a10b6a6a902b7bfd972ee6fe58a82
Jan Krystof <jan.krystof@.ibm.com>
2026-03-02 23:06:07 +0100

    SRVLOGIC-807 | towards CI run

--- run_container.sh
+++ run_container.sh
@@ -12,6 +12,8 @@ THIS_SCRIPT_DIR="$(dirname $(realpath "$0"))"
 : "${SSH_KEY_LOCATION:?SSH_KEY_LOCATION is not set}"
 : "${ANSIBLE_DIR_HOST:=$THIS_SCRIPT_DIR/ansible}"
 : "${CONTAINER_IMAGE:=quay.io/rh-ee-jkrystof/osl_cut_off_automation:latest}"
+: "${GIT_USER_NAME:=not set}"
+: "${GIT_USER_EMAIL:=not@set}"
 
 if [[ ! -d "$CLONE_OUT_HOST" ]]; then
   echo "Host directory CLONE_OUT_HOST does not exist: $CLONE_OUT_HOST"
@@ -29,38 +31,45 @@ if [[ ! -f "$SSH_KEY_LOCATION" ]]; then
 fi
 
 
-# prepare configuration for git
-TMP_SSH_DIR=$(mktemp -d)
+TEMP_HOME=$(mktemp -d /tmp/ci_home.XXX)
+chmod 700 "$TEMP_HOME"
+echo Temporary HOME created in $TEMP_HOME
+
+TMP_SSH_DIR="$TEMP_HOME/.ssh"
+mkdir -p "$TMP_SSH_DIR"
 chmod 700 "$TMP_SSH_DIR"
 
-# copy ssh private key
 cp "$SSH_KEY_LOCATION" "$TMP_SSH_DIR/id_rsa"
 chmod 600 "$TMP_SSH_DIR/id_rsa"
 
-ssh-keyscan github.com >> "$TMP_SSH_DIR/known_hosts" 2>/dev/null
+ssh-keyscan github.com > "$TMP_SSH_DIR/known_hosts" 2>/dev/null
+chmod 644 "$TMP_SSH_DIR/known_hosts"
+
 GIT_SSH_COMMAND="ssh -i $TMP_SSH_DIR/id_rsa -o UserKnownHostsFile=$TMP_SSH_DIR/known_hosts -o StrictHostKeyChecking=yes"
 
 docker run --rm \
+  --user $(id -u):$(id -g) \
+  -e HOME="$TEMP_HOME" \
+  -e ANSIBLE_REMOTE_TMP="$TEMP_HOME/.ansible/tmp" \
   -e GIT_SSH_COMMAND="$GIT_SSH_COMMAND" \
-  -v "${TMP_SSH_DIR}:${TMP_SSH_DIR}:ro" \
-  -v "${CLONE_OUT_HOST}:/tmp/osl_cut_off_automation/OUT" \
-  -v "${ANSIBLE_DIR_HOST}:/tmp/osl_cut_off_automation/ansible:ro" \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
+  -v "$TEMP_HOME:$TEMP_HOME" \
+  -v "$CLONE_OUT_HOST:/tmp/osl_cut_off_automation/OUT" \
+  -v "$ANSIBLE_DIR_HOST:/tmp/osl_cut_off_automation/ansible:ro" \
   $CONTAINER_IMAGE \
   bash -c '
     set -euo pipefail
-
-    echo "Preparing SSH environment..."
-    # Setup git identities
-    git config --global user.name "pepa zdepa"
-    git config --global user.email "pepa@z.depa"
-
-    echo "Mounted clone directory:"
+    echo "HOME=$HOME"
+    echo "TMP_SSH_DIR=$HOME/.ssh"
+    ls -la "$HOME/.ssh"
+    git --version
+    # example git clone
+    echo aaa
+    git config --global user.name "'"$GIT_USER_NAME"'"
+    git config --global user.email "'$GIT_USER_EMAIL'"
+    git config --global user.name
+    git config --global user.email
     cd /tmp/osl_cut_off_automation/ansible
-    echo ...OUT
-    ls -la /tmp/osl_cut_off_automation/OUT
-
-    echo ..ansible
-    ls -la /tmp/osl_cut_off_automation/ansible
-
     ansible-playbook play__osl_cut_off.yml -e cloning_target_dir=/tmp/osl_cut_off_automation/OUT
   '
